127a33b1e7627fdf03862fb810b846b9eeea4518
Marcos Caballero <mcaballero@itainnova.es>
2023-01-30 19:32:12 +0100

    #2 Implemented 4 unit tests

---
+++ src/main/test/datagencars/surprise/test_surprise.py
@@ -0,0 +1,33 @@
+import surprise
+from src.main.python import rs_surprise
+import unittest
+import logging
+
+class TestCreateAlgorithm(unittest.TestCase):
+    def test_valid_algorithm_name(self):
+        algo_name = "SVD"
+        params = {"n_factors": 100, "n_epochs": 20, "lr_all": 0.005, "reg_all": 0.02}
+        result = rs_surprise.create_algorithm(algo_name, params)
+        self.assertIsInstance(result, surprise.SVD)
+
+    def test_invalid_algorithm_name(self):
+        algo_name = "InvalidAlgorithm"
+        params = None
+        with self.assertRaises(ValueError) as context:
+            rs_surprise.create_algorithm(algo_name, params)
+        self.assertEqual(str(context.exception), "Invalid algorithm name")
+
+class TestCreateSplitStrategy(unittest.TestCase):
+    def test_valid_split_strategy(self):
+        strategy = "KFold"
+        params = {"n_splits": 5, "shuffle": True}
+        result = rs_surprise.create_split_strategy(strategy, params)
+        self.assertIsInstance(result, surprise.model_selection.KFold)
+
+    def test_invalid_split_strategy(self):
+        strategy = "InvalidStrategy"
+        params = None
+        with self.assertRaises(ValueError) as context:
+            rs_surprise.create_split_strategy(strategy, params)
+        self.assertEqual(str(context.exception), 'Invalid split strategy')
+
