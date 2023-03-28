33da32cb8ba321f4efad0b941aa847a479d0d8ba
Marcos Caballero <mcaballero@itainnova.es>
2023-03-28 11:01:49 +0200

    #3 scikitlearn_helpers.py Features, Folds, Metrics and Split

---
+++ src/main/python/datagencars/evaluation/scikitlearn_helpers.py
@@ -0,0 +1,161 @@
+import time
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cluster import DBSCAN, KMeans
+from sklearn.metrics import (
+    make_scorer,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+)
+from sklearn.model_selection import (
+    cross_validate,
+    ShuffleSplit,
+    KFold,
+    LeaveOneOut,
+    RepeatedKFold,
+    train_test_split,
+)
+from sklearn.impute import SimpleImputer
+
+def create_algorithm(algo_name, params=None):
+    """
+    Creates an algorithm from its name and parameters
+    :param algo_name: the name of the algorithm
+    :param params: the parameters of the algorithm
+    :return: the algorithm
+    """
+    if algo_name == "KNeighborsClassifier":
+        return KNeighborsClassifier(**params)
+    elif algo_name == "SVC":
+        return SVC(**params)
+    elif algo_name == "GaussianNB":
+        return GaussianNB()
+    elif algo_name == "RandomForestClassifier":
+        return RandomForestClassifier(**params)
+    elif algo_name == "KMeans":
+        return KMeans(**params)
+    elif algo_name == "DBSCAN":
+        return DBSCAN(**params)
+    else:
+        raise ValueError("Invalid algorithm name")
+    
+def create_split_strategy(strategy, strategy_params):
+    """
+    Creates a split strategy from its name and parameters
+    :param strategy: the name of the strategy
+    :param strategy_params: the parameters of the strategy
+    :return: the strategy
+    """
+    if strategy == "ShuffleSplit":
+        return ShuffleSplit(n_splits=strategy_params['n_splits'],
+                             train_size=strategy_params['train_size'],
+                             random_state=strategy_params['random_state'])
+    elif strategy == "KFold":
+        return KFold(n_splits=strategy_params['n_splits'],
+                     random_state=strategy_params['random_state'],
+                     shuffle=strategy_params['shuffle'])
+    elif strategy == "LeaveOneOut":
+        return LeaveOneOut()
+    elif strategy == "RepeatedKFold":
+        return RepeatedKFold(n_splits=strategy_params['n_splits'],
+                             n_repeats=strategy_params['n_repeats'],
+                             random_state=strategy_params['random_state'])
+    elif strategy == "train_test_split":
+        return train_test_split # Note that train_test_split is a function and not a class, so it doesn't need instantiation.
+    else:
+        raise ValueError(f"Unsupported split strategy: {strategy}")
+
+def get_categorical_cols(df):
+    '''
+    Returns a list of the column names in a pandas dataframe that are categorical.
+    '''
+    # Select columns with dtype 'object'
+    categorical_cols = df.select_dtypes(include=['object']).columns.tolist()
+    
+    # Check if there are any columns with dtype 'category'
+    categorical_cols += df.select_dtypes(include=['category']).columns.tolist()
+    
+    return categorical_cols
+
+def preprocess_missing_values(df, strategy='mean'):
+    '''
+    Imputes missing values in a pandas dataframe.
+    Replace 'mean' with 'median', 'most_frequent', or 'constant' if needed
+    '''
+    imputer = SimpleImputer(missing_values=np.nan, strategy=strategy)
+    imputed_data = imputer.fit_transform(df)
+    return pd.DataFrame(imputed_data, columns=df.columns)
+
+def evaluate(item_df, context_df, rating_df, algo_list, strategy_instance, metrics, user_id=None):
+    """
+    Evaluates a list of algorithms using a given split strategy
+    :param item_df: the item dataframe
+    :param context_df: the context dataframe
+    :param rating_df: the rating dataframe
+    :param algo_list: the list of algorithms to evaluate
+    :param strategy_instance: the split strategy to use
+    :param metrics: the metrics to use
+    :param user_id: the target user id
+    :return: a dataframe with the results
+    """
+    try:
+        merged_df = rating_df.merge(item_df, on='item_id').merge(context_df, on='context_id')
+    except KeyError as e:
+        return f"The rating, user, item and context datasets don´t have '_id' columns in common. {e}"
+    categorical_columns = get_categorical_cols(merged_df)
+    dummies_df = pd.get_dummies(merged_df, columns=categorical_columns)
+    data = preprocess_missing_values(dummies_df, strategy='mean')
+    # Filter the data for the target user if a target_user_id is provided
+    if user_id is not None:
+        data = data[data['user_id'] == user_id]
+
+    # Prepare the input for the evaluation
+    X = data.drop(columns=["user_id", "item_id", "rating"])
+    y = data["rating"]
+
+    # Prepare the dictionary for the metrics
+    scoring = {}
+    for metric in metrics:
+        if metric == "Precision":
+            scoring["precision"] = make_scorer(precision_score, average='weighted')
+        elif metric == "Recall":
+            scoring["recall"] = make_scorer(recall_score, average='weighted')
+        elif metric == "F1 score":
+            scoring["f1"] = make_scorer(f1_score, average='weighted')
+        elif metric == "ROC-AUC":
+            scoring["roc_auc"] = make_scorer(roc_auc_score, needs_proba=True, average='weighted', multi_class='ovr')
+
+    results = []
+    for algorithm in algo_list:
+        algo_name = type(algorithm).__name__
+        cv_results = cross_validate(algorithm, X, y, cv=strategy_instance, scoring=scoring, return_estimator=True)
+        n_splits = strategy_instance.get_n_splits()
+        for fold_idx in range(n_splits):
+            fold_results = {"Fold": fold_idx + 1, "Algorithm": algo_name}
+            for metric, score in cv_results.items():
+                if metric.startswith("test_precision"):
+                    fold_results["Precision"] = score[fold_idx]
+                elif metric.startswith("test_recall"):
+                    fold_results["Recall"] = score[fold_idx]
+                elif metric.startswith("test_f1"):
+                    fold_results["F1 score"] = score[fold_idx]
+                elif metric.startswith("test_roc_auc"):
+                    fold_results["ROC-AUC"] = score[fold_idx]
+                elif metric.startswith("fit_time"):
+                    fold_results["Time (train)"] = score[fold_idx]
+                elif metric.startswith("score_time"):
+                    fold_results["Time (test)"] = score[fold_idx]
+            results.append(fold_results)
+
+    results_df = pd.DataFrame(results)
+    # Reorder columns
+    column_order = ["Fold", "Algorithm", "Precision", "Recall", "F1 score", "ROC-AUC", "Time (train)", "Time (test)"]
+    results_df = results_df[column_order]
+
+    return results_df
