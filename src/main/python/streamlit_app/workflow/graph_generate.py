854d7e76f570e1d550b08641022f8318eddf7e99
bfranco <bfranco@itainnova.es>
2023-04-25 10:52:17 +0200

    #4 update graph-creation

--- src/main/python/streamlit_app/workflow/graph_generate.py
+++ src/main/python/streamlit_app/workflow/graph_generate.py
@@ -28,7 +28,12 @@ class Workflow:
                             if json_opt_params['init_step'] == 'True':
                                 g.node(node['node_name'], color = 'red', shape='rect')
                             else:
-                                if json_opt_params[node['condition']] == 'True':
+                                draw_node = True
+                                for condition in node['condition']:
+                                    if json_opt_params[condition[0]] != condition[1]:
+                                        draw_node = False
+                                        break
+                                if draw_node:
                                     g.node(node['node_name'], shape='rect')
 
                         else:
@@ -58,18 +63,18 @@ class Workflow:
                     else:
                         draw_arrow = True
                         for condition in arrow['condition']:
-                            print(condition)
                             if json_opt_params[condition[0]] != condition[1]:
                                 draw_arrow = False
                                 break
                     if draw_arrow:
                         label = (arrow['label'][1:-1].split(','))
-                        print(label)
                         if 'CARS' in json_opt_params.keys() and json_opt_params['CARS'] == 'False':
                             for elem in self.CARS_remove:
                                 if elem in arrow['label']:
                                     label.remove(elem)
                         label_str = str(label)
+                        if label_str.replace('[','').replace(']','').replace('\'','') == 'F' and json_opt_params['init_step'] == 'False':
+                            label_str = '['+json_opt_params['file']+']'
                         g.edge(arrow['init_node'], arrow['end_node'], label=label_str.replace('\'',''))
 
 
@@ -79,10 +84,22 @@ class Workflow:
                     legend_text = '''<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
                                                 <TR><TD PORT="files" COLSPAN="2">Files</TD></TR>'''
                     for legend_item in workflow['legend']:
+                        quit = False
+                        legend_item_text = data["legend"][legend_item]
                         if 'CARS' in json_opt_params.keys() and json_opt_params['CARS'] == 'False' and legend_item in self.CARS_remove:
                             pass
                         else:
-                            legend_text = legend_text + '''<TR><TD ALIGN="LEFT">'''+legend_item+'''</TD><TD ALIGN="LEFT">'''+data["legend"][legend_item]+'''</TD></TR>'''
+                            if legend_item == 'F':
+                                if  json_opt_params['init_step'] == 'False':
+                                    legend_item = json_opt_params['file']
+                                    legend_item_text = data["legend"][legend_item]
+                                    quit = True
+                                else: 
+                                    if json_opt_params['CARS'] == 'False' and json_opt_params['init_step'] == 'True':
+                                        legend_item_text = 'File from U, I'
+                            legend_text = legend_text + '''<TR><TD ALIGN="LEFT">'''+legend_item+'''</TD><TD ALIGN="LEFT">'''+legend_item_text+'''</TD></TR>'''
+                        if quit:
+                            break
                     legend_text = legend_text + '''</TABLE>>'''
                     legend.node('legend', legend_text)
 
@@ -97,6 +114,7 @@ if __name__ == "__main__":
     wf = Workflow()
     json_opt_params = {}
     json_opt_params['CARS'] = "True"
-    json_opt_params['NULLValues'] = "True"
-    json_opt_params['init_step'] = "True"
-    print(wf.create_workflow('ReplicateDataset', json_opt_params))
+    json_opt_params['Num2Cat'] = "False"
+    json_opt_params['init_step'] = "False"
+    json_opt_params['file'] = "C"
+    print(wf.create_workflow('MappingToCategorization', json_opt_params))
