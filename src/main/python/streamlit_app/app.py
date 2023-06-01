d87c42f0d324b3c3d1fdaab017f40939b310b434
bfranco <bfranco@itainnova.es>
2023-06-01 15:13:49 +0200

    #4 wf recalculateratings update

--- src/main/python/streamlit_app/app.py
+++ src/main/python/streamlit_app/app.py
@@ -680,16 +680,32 @@ elif general_option == 'Pre-process a dataset':
         st.write('TODO')
     elif is_preprocess == 'Recalculate ratings': 
         # Loading dataset:
-        init_step = 'True'        
-        _, _, _, rating_df = util.load_dataset(file_type_list=['rating'])
+        init_step = 'True' 
+        _, item_df, context_df, rating_df = util.load_dataset(file_type_list=['item', 'context', 'rating'])
 
         # WF --> Recalculate ratings:
         st.header('Apply workflow: Recalculate ratings')
         # Help information:
         help_information.help_recalculate_ratings_wf()
         # Showing the initial image of the WF:
-        workflow_image.show_wf(wf_name='RecalculateRatings', init_step='True', with_context=True, optional_value_list=[('NULLValues', 'True')])
+        workflow_image.show_wf(wf_name='RecalculateRatings', init_step='True', with_context=True, optional_value_list=[('NULLValues', str(True)), ('NULLValuesC', str(True)), ('NULLValuesI', str(True))])
 
+        # Options tab:
+        tab_replace_null_values, tab_generate_user_profile, tab_extend_dataset  = st.tabs(['Replace NULL values', 'Generate user profile', 'Extend dataset'])     
+        # REPLACE NULL VALUES TAB:
+        new_item_df = pd.DataFrame()
+        new_context_df = pd.DataFrame()        
+        with tab_replace_null_values:
+            null_values_c, null_values_i = util.tab_logic_replace_null('RecalculateRatings', with_context, item_df, context_df)
+        with tab_generate_user_profile:
+            optional_value_list = [('NULLValues', str(null_values_c & null_values_i)), ('NULLValuesC', str(null_values_c)), ('NULLValuesI', str(null_values_i))]
+            if with_context:
+                user_profile_df = util.tab_logic_generate_up('ExtendDataset', with_context, optional_value_list, rating_df, new_item_df, new_context_df)         
+            else:    
+                user_profile_df = util.tab_logic_generate_up('ExtendDataset', with_context, optional_value_list, rating_df, new_item_df) 
+        with tab_extend_dataset:
+            # Showing the current image of the WF:
+            workflow_image.show_wf(wf_name='ExtendDataset', init_step='False', with_context=with_context, optional_value_list=[('NULLValues', str(null_values_c or null_values_i)), ('NULLValuesC', str(null_values_c)), ('NULLValuesI', str(null_values_i))])
         st.write('TODO')
     elif is_preprocess == 'Replace NULL values':
         if with_context:
