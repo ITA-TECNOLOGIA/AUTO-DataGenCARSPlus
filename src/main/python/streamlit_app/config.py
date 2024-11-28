96ad97f42404f1910c418bf1c1edb1960d119474
mcrodriguez <mcrodriguez@itainnova.es>
2024-11-28 10:02:25 +0100

    Update General Title

--- src/main/python/streamlit_app/config.py
+++ src/main/python/streamlit_app/config.py
@@ -1,5 +1,5 @@
 #### App Streamlit settings ####:
-APP_TITLE = 'AUTO-DataGenCARS+: Advanced Pyton-Based User orienTed tOol DataGenCARS'
+APP_TITLE = 'AUTO-DataGenCARS+: Advanced Python-Based User orienTed tOol DataGenCARS'
 APP_ICON = 'resources/icons/logo-autodatagencarsplus.png'
 APP_LAYOUT = ['centered', 'centered', 'wide']
 APP_INITIAL_SIDEBAR_STATE = ['auto', 'expanded', 'auto', 'collapsed']
